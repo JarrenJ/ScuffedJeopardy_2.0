cc362ff6804ced15ac34ac20b3fbca5c758f99e8
JarrenJ <j.jarren@yahoo.com>
2020-08-19 20:48:23 -0500

    Updates

---
+++ precache-manifest.a224d384ef889f942b13eef11c49ad49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792a013e608e6898519f3c77af469b10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d65dbe318999778c6d2f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6070cc80c73df4298d6f",
+    "url": "/static/js/2.8669c5b2.chunk.js"
+  },
+  {
+    "revision": "9caebd9c6bad93f2eb60fdf2a2eb09a2",
+    "url": "/static/js/2.8669c5b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65dbe318999778c6d2f",
+    "url": "/static/js/main.86c52b83.chunk.js"
+  },
+  {
+    "revision": "900a1ca645281ecd1203",
+    "url": "/static/js/runtime-main.331a4112.js"
+  },
+  {
+    "revision": "1fc2a9581a67376b2cba9d543b4256b0",
+    "url": "/static/media/final.1fc2a958.jpeg"
+  }
+]);
